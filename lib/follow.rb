20828cfb7725ee0711ad4bb24453adfaa8a11388
JeremyMarc232 <jeremymarc@hotmail.fr>
2020-01-22 17:38:28 +0100

    Essai sur follow mais too many requests

---
+++ lib/follow.rb
@@ -0,0 +1,23 @@
+require 'dotenv'# Appelle la gem Dotenv
+require 'twitter'
+
+def return_client_object()
+  Dotenv.load('.env')
+ client = Twitter::REST::Client.new do |config|
+   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ end
+ return client
+end
+client = return_client_object
+
+def follow_bonjour_monde(client)
+  results = client.search("#bonjour_monde", result_type: "recent").take(20)
+  results.each do |tweet|
+    client.follow(:screen_name => tweet.user.screen_name)
+  end
+end
+
+follow_bonjour_monde(client)
